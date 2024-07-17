98bbba923938eb24da5521fa0e91c457a9272efd
yymin1022 <yymin1022@gmail.com>
2024-07-17 16:04:23 +0900

    Update Project : Added RTSPClient Dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,12 +36,14 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
+    implementation("com.github.am3n:RTSP-Client-Android:4.1.0")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
